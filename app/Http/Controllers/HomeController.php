0bf81dc5ea30c6b34817d54863d0a59f1a8ce63a
Maxsey <mwl@live.com>
2018-05-16 18:40:22 +0800

    add docs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        return view('dashboard');
     }
 
     public function dashboard()
