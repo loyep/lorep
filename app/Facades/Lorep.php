a2bda5470cb07b3f42c1568012fcba1ed92a6551
Maxwell Liu <mwl@live.com>
2018-05-23 23:46:09 +0800

    update

---
+++ app/Facades/Lorep.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxwell
+ * Date: 2018/5/23
+ * Time: 下午11:45
+ */
+
+namespace App\Facades;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class Lorep extends Facade
+{
+
+}
